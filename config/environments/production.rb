7c36316443f47473762585460bd0a3e08046d5a0
EC2 Default User <ec2-user@ip-172-31-45-59.ap-northeast-1.compute.internal>
2020-05-06 14:41:46 +0000

    herokuでのエラー解決試し

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.default_url_options = { protocol: 'https', host: 'https://8d5200b8165c4d5a83a9a078758aad59.vfs.cloud9.ap-northeast-1.amazonaws.com/' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => "hiromuhappy0405@gmail.com", #先ほど作成した送信用gmailアドレス
+    :password => "sithwnibsvxvdodj", #2段階認証したアカウントで発行したアプリパスワード
+    :authentication => 'login'
+  }
 end
